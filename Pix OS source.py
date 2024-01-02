a7b078bd00067feec49a60ad6c8f31a2d8881c1b
megg-at-github <151755668+megg-at-github@users.noreply.github.com>
2024-01-03 01:08:40 +0530

    Update Pix OS source.py

--- Pix OS source.py
+++ Pix OS source.py
@@ -3,12 +3,13 @@ print('Welcome to Pix OS')
 running = input('Do you want to run this OS? ')
 if running != 'yes': 
     quit()
+    
 print('Ok, running OS :)') 
 
 running = input('Enter Password ')
 if running != 'PLACEHOLDER.PASSWORD':
     quit()
-
+    
 import datetime
 
 from battery_status import get_battery_info 
