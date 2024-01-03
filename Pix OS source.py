1145b3f088205ce5c5aab62d61e872cafae3fb56
megg-at-github <151755668+megg-at-github@users.noreply.github.com>
2024-01-04 01:46:17 +0530

    Update Pix OS source.py

--- Pix OS source.py
+++ Pix OS source.py
@@ -9,12 +9,15 @@ print('Ok, running OS :)')
 running = input('Enter Password ')
 if running != 'PLACEHOLDER.PASSWORD':
     quit()
-    
+
 import datetime
+from battery_status import get_battery_info
 
-from battery_status import get_battery_info 
+from pc_info import get_pc_info
 
-print(datetime.datetime.now()) 
+print(datetime.datetime.now())
+
+print(get_pc_info())
 
 if running != 'time':
     quit()
@@ -23,6 +26,7 @@ print(datetime.datetime.now())
 # Within your program's logic, where you want to display the battery status:
 battery_info = get_battery_info()
 print(battery_info)  # Or use the information as needed in your program
+
 
 
 
